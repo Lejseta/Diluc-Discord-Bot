92d195cbea3dd0769d15c5e8c8d35a41d3857218
Darcy <32802467+Lejseta@users.noreply.github.com>
2022-09-11 23:51:42 +0100

    Update

--- Commands/Utility.cs
+++ Commands/Utility.cs
@@ -99,8 +99,6 @@ namespace Bot.Commands
                 await connectedChannel.DisconnectAsync();
                 return;
             }
-           
-
             do
             {
                 await Task.Delay(1000);
